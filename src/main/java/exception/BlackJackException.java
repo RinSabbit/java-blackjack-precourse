e643241c14fbc0001054c2d64da4b3f2483adad2
RinSabbit <celena1004@naver.com>
2020-12-16 15:29:00 +0900

    feat: 커스텀 예외 처리 기능 구현

---
+++ src/main/java/exception/BlackJackException.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class BlackJackException extends IllegalArgumentException {
+
+    public BlackJackException(String message){
+        super("[ERROR] " + message);
+    }
+
+}
